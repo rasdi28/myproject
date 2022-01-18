c74e8b9c2594ff9cb6f45c2d71db896a478a0194
Rasdi Abdulrohman <rasdi.abdulrohman15120@student.unsika.ac.id>
2022-01-18 23:28:38 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { IndexComponent } from './post/index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    IndexComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
